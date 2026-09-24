7405bafec304f6e8a300ea1b87a2b854a1517588
Fbgg2k <felipeoverkill1@gmail.com>
2026-09-24 15:37:37 -0300

    chore: mantém apenas documentação na main

--- src/routes/empresaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getEmpresa } = require('../controllers/empresaController')
-
-const empresaRoutes = express.Router()
-
-empresaRoutes.get('/:cnpj', getEmpresa)
-
-module.exports = empresaRoutes
